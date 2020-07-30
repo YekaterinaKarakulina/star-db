f32c5da0c473021bcdbdc082ed0482cdcb305ac6
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-07-30 17:59:29 +0400

    feat: create star DB components

--- src/index.js
+++ src/index.js
@@ -1,47 +1,6 @@
-class SwapiService {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-  _apiBase = 'https://swapi.dev/api';
-
-  async getData(url) {
-    const res = await fetch (`${this._apiBase}${url}`);
-  
-    if(!res.ok) {
-      throw new Error(`Could not fetch ${url}, received ${res.status}`)
-    }
-  
-    return await res.json();
-  };
-
-  async getAllPeople() {
-    const res = await this.getData(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getData(`/people/${id}`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getData(`/planets/`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getData(`/planets/${id}`);
-  }
-
-  async getAllStarships() {
-    const res = await this.getData(`/starships/`);
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getData(`/starships/${id}`);
-  }
-
-}
-
-const swapi = new SwapiService();
-swapi.getPerson(3).then((data) => {
-  console.log(data);
-})
+ReactDOM.render(<App />,
+  document.getElementById('root'));
