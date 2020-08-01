04f35574cf239c5b5df8ae636388319067ca53a3
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-08-01 11:55:40 +0400

    feat: add getData to pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,10 +4,15 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import PeoplePage from '../people-page';
 import ErrorIndicator from '../error-indicator';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+import SwapiService from '../../services/swapi-service';
 
 import './app.css';
 
 export default class App extends React.Component {
+
+  swapiService = new SwapiService();
 
   state = {
     hasError: false
@@ -28,6 +33,29 @@ export default class App extends React.Component {
         <Header />
         <RandomPlanet />
         <PeoplePage />
+
+        <div className="row mb-3">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 
 import './item-list.css';
-import SwapiService from '../../services/swapi-service';
 import Spinner from '../spinner';
 
 export default class ItemList extends Component {
 
-  swapiService = new SwapiService();
-
   state = {
-    peopleList: null
+    itemList: null
   }
 
   componentDidMount() {
-    this.swapiService
-      .getAllPeople()
-      .then((peopleList) => {
+
+    const { getData } = this.props;
+    console.log(getData);
+
+    getData()
+      .then((itemList) => {
+        console.log(itemList)
         this.setState({
-          peopleList
+          itemList
         });
       });
   }
@@ -36,13 +37,14 @@ export default class ItemList extends Component {
 
   render() {
 
-    const { peopleList } = this.state;
+    const { itemList } = this.state;
+    console.log(itemList);
 
-    if (!peopleList) {
+    if (!itemList) {
       return <Spinner />
     }
 
-    const people = this.renderItems(peopleList);
+    const people = this.renderItems(itemList);
 
     return (
       <ul className="item-list list-group">
